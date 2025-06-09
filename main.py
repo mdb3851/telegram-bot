6ebb0b0cbd2cbd6a9d183b2cd02e69e1279e64c5
msb3851 <103892512+mdb3851@users.noreply.github.com>
2025-06-09 16:41:54 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
 from telegram import Update
-from telegram.ext import ContextTypes
-import os
-
-TOKEN = os.environ["BOT_TOKEN"]
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام هستی جان! ربات فعاله 🦋")
+    await update.message.reply_text("سلام! من آنلاینم 🌟")
 
-app = ApplicationBuilder().token(TOKEN).build()
+app = ApplicationBuilder().token("7721995609:AAHFik1G49bu0OACtFWpv_NBHDzOESxVtTI").build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
